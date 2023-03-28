8770a002900090e0866561d7c995ea90ed4d1d3d
pragyan borah <pragyanborah@pragyans-MacBook-Air.local>
2023-03-29 00:34:23 +0530

    initial commit of the web

---
+++ vite-project/src/components/index.js
@@ -0,0 +1,34 @@
+import Billing from "./Billing";
+import Business from "./Business";
+import Button from "./Button";
+import CardDeal from "./CardDeal";
+import CTA from "./CTA";
+import Feedback from "./Feedback";
+import Footer from "./Footer";
+import GetStarted from "./GetStarted";
+import Hero from "./Hero";
+import Navbar from "./Navbar";
+import Stats from "./Stats";
+import Testimonials from "./Testimonials";
+import Clients from "./Clients"
+
+
+
+export {
+    Billing ,
+    Business ,
+    Button,
+    CardDeal,
+    CTA,
+    Feedback,
+    Footer ,
+    GetStarted,
+    Hero,
+    Navbar,
+    Stats,
+    Testimonials,
+    Clients
+    
+
+}
+
